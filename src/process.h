506c325ef2606fb9478c84b17946e6f5c97a5f5b
jspangled <jes3277@rit.edu>
2021-11-16 18:22:45 -0500

    Fix spacing in process.h

--- src/process.h
+++ src/process.h
@@ -14,34 +14,35 @@ typedef enum { CPU, IO } ProcessType;
 
 // Process struct
 typedef struct { 
-	int pid; 
-	int noi; 
-	int priority; 
-	int sleep_time; 
-	double sleep_prob; 
-	ProcessState state; 
-	ProcessType type; 
+    int pid; 
+    int noi; 
+    int priority; 
+    int sleep_time; 
+    double sleep_prob; 
+    ProcessState state; 
+    ProcessType type; 
 } Process;
 
 void cpu_proc(Process* proc) {
-	for (int i = 0; i < proc->noi; i++) {
-		;
-	}
+    pritnf("PID: %d, Going for CPU", proc->pid);
+    for (int i = 0; i < proc->noi; i++) {
+        ;
+    }
 }
 
 void io_proc(Process* proc) {
-	srand(time(0));
-	for (int i = 0; i < proc->noi; i++) {
-		printf("%d, loop iteration %d", proc->pid, i);
-		double prob = ((double) rand() / (RAND_MAX)) + 1;
-		if (prob < proc->sleep_prob) {
-			// Send IO signal to scheduler
-			printf("PID: %d Going for IO", proc->pid);
-			sleep(proc->sleep_time);
-			printf("PID: %d Back from IO", proc->pid);	
-			// Inform schedular via MSG Q
-		}	
-	}
+    srand(time(0));
+    for (int i = 0; i < proc->noi; i++) {
+        printf("%d, loop iteration %d", proc->pid, i);
+        double prob = ((double) rand() / (RAND_MAX)) + 1;
+        if (prob < proc->sleep_prob) {
+            // Send IO signal to scheduler
+            printf("PID: %d Going for IO", proc->pid);
+            sleep(proc->sleep_time);
+            printf("PID: %d Back from IO", proc->pid);  
+            // Inform schedular via MSG Q
+        }   
+    }
 }
 
 #endif
